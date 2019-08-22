333dc0ba36e9553e860f4c50461d3770944287ad
alabra <alllexe@mail.ru>
2019-08-22 08:54:36 +0300

    hw 10.3.3

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -1,13 +1,18 @@
 package ru.javawebinar.topjava.web.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
@@ -16,6 +21,9 @@ import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 @RequestMapping(ProfileRestController.REST_URL)
 public class ProfileRestController extends AbstractUserController {
     static final String REST_URL = "/rest/profile";
+
+    @Autowired
+    UserFormValidator userFormValidator;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
@@ -41,7 +49,11 @@ public class ProfileRestController extends AbstractUserController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update(@RequestBody UserTo userTo) {
+    public void update(@Valid @RequestBody UserTo userTo, BindingResult result) {
+        userFormValidator.validate(userTo, result);
+        if (result.hasErrors()) {
+            throw new IllegalRequestDataException(ValidationUtil.bindingResultErrorsMessage(result));
+        }
         super.update(userTo, authUserId());
     }
 
